f772d2e73be213d6ae233471cef3c18ae69b45db
Bryce Avot <avotgames@gmail.com>
2026-04-24 14:29:06 -0400

    made changes

---
+++ vault/notes/programming/java/mooc.fi/tmc/mooc-java-programming-i/part01-part01_05.message/src/main/java/message.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Message {
+
+    public static void main(String[] args) {
+        Scanner shitty_ass_scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+
+        String stupid_ass_message = shitty_ass_scanner.nextLine();
+
+        System.out.println(stupid_ass_message);
+
+    }
+}
